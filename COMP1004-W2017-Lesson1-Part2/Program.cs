bfa6d97f10c86c8b43accea4e639df2cacc9a0ad
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-01-09 14:48:15 -0500

    added Person class

--- COMP1004-W2017-Lesson1-Part2/Program.cs
+++ COMP1004-W2017-Lesson1-Part2/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace COMP1004_W2017_Lesson1_Part2
 {
+    
+
+
     /// <summary>
     /// Driver class for our console application
     /// </summary>
@@ -13,6 +16,13 @@ namespace COMP1004_W2017_Lesson1_Part2
     {
         static void Main(string[] args)
         {
+            Person Tom = new Person("Tom", 30);
+            Tom.SaysHello();
+
+            Console.WriteLine();
+
+            Person Jane = new Person("Jane", 26);
+            Jane.SaysHello();
         }
     }
 }
